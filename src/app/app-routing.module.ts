10f02738e4a7de9ff8a259fc2c91c0737a0189a5
Bruja <sebaspe12345@gmail.com>
2020-04-06 21:46:26 -0500

    upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { ProyectoInvestigacionComponent } from './Investigacion/home/proyecto-in
 
 
 const routes: Routes = [
+  {path:'',redirectTo:"home", pathMatch:"full"},
   {
     path: "home",component:HomeComponent
   },
